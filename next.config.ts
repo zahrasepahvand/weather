8fbc52ddfa3fd10bb9c84f481a80e38ab2832a34
Bahar <zahrasepahvand30@gmail.com>
2025-08-25 23:01:48 +0200

    working api

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true }, // Required if you're using <Image />
-  basePath: '/weather',        // Replace with your repo name
+  basePath: isProd ? '/weather' : '',     // Replace with your repo name
   trailingSlash: true  
   /* config options here */
 };
