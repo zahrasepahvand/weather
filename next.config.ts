fc75ef49164e55a30860345c96f06ee1d25e6727
Bahar <zahrasepahvand30@gmail.com>
2025-08-25 21:30:14 +0200

    .yml version part fixed

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true }, // Required if you're using <Image />
-  basePath: '/REPO_NAME',        // Replace with your repo name
+  basePath: '/weather',        // Replace with your repo name
   trailingSlash: true  
   /* config options here */
 };
